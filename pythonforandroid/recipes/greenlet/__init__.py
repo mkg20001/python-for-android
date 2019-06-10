5982449e2b937ecc2113157027df8e0d90a4562b
Maciej Krüger <mkg20001@gmail.com>
2019-06-10 14:43:03 +0200

    [tmp] downgrade greenlet to v0.4.14 for zn

--- pythonforandroid/recipes/greenlet/__init__.py
+++ pythonforandroid/recipes/greenlet/__init__.py
@@ -2,7 +2,7 @@ from pythonforandroid.recipe import CompiledComponentsPythonRecipe
 
 
 class GreenletRecipe(CompiledComponentsPythonRecipe):
-    version = '0.4.15'
+    version = '0.4.14'
     url = 'https://pypi.python.org/packages/source/g/greenlet/greenlet-{version}.tar.gz'
     depends = ['setuptools']
     call_hostpython_via_targetpython = False
